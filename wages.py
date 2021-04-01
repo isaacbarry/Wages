ad8994ab7cd676a071c3800ff0bf781b7aac6aca
Isaac 2.0 <isaac.barry@my.jcu.edu.au>
2021-04-01 12:19:59 +1000

    Added input for user to state if they are on salary or overstay

--- wages.py
+++ wages.py
@@ -5,8 +5,8 @@ def main():
     # ask user if adding new user
     # if adding new user add them to a csv and ask what their salary is
     check = False
-    add_another = False
-    while not add_another:
+    add_another = True
+    while add_another:
         while not check:
             print('Do you want to add a new user? Yes/No')
             user_add = input().upper()
@@ -15,10 +15,23 @@ def main():
             check = bool(user_add[1])
         # if adding new user get details of new user
         if user_add:
-            print('Salary of new user')
-            salary = int(input('What is their salary?'))
+            check = False
+            while not check:
+                # Check to see if user is salary or overstay
+                response = input('Are they on salary or overstay?').upper()
+                response = check_response(response)
+                overstay = response[0]
+                salary = response[1]
+                check = response[2]
         else:
             add_another = False
+            print('NO')
+        check = False
+        while not check:
+            add_another = input('Do you want to add another user? yes/no').upper()
+            temp = check_response(add_another)
+            add_another = temp[0]
+            check = bool(temp[1])
 
 
 def check_input(user_add):
@@ -35,6 +48,25 @@ def check_input(user_add):
         check = False
 
     return user_add, check
+
+
+def check_response(response):
+    """Checks input if it is overstay or salary"""
+    overstay = 0
+    salary = 0
+    if response == 'OVERSTAY':
+        overstay = input('Amount per day')
+        check = True
+    elif response == 'SALARY':
+        salary = input('What is their yearly salary?')
+        check = True
+    else:
+        print('Please enter overstay or salary')
+        check = False
+
+    return overstay, salary, check
+
+
 
 
 if __name__ == '__main__':
